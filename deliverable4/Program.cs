ae77cd3c330ff976ada41ef37af9e6b3d3cb1105
owner <owner@DESKTOP-C9TTQOE>
2023-02-02 18:14:53 -0500

    Add project files.

---
+++ deliverable4/Program.cs
@@ -0,0 +1,33 @@
+﻿/* 
+ Author: Victor Minaya
+ Date: 2/2/2023
+ Comments: This C# Consolole application code demostrates the use 
+           of Arrays after getting input from users
+*/
+
+using System;
+
+namespace deliverable4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+            // Initialize fibonacci series
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < fibonacci.Length; i++)
+            {
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }        
+        }
+    }
+}
